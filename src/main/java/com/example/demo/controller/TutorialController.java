3b9b05e7b6234c5c00d69ac5e548c9d6531344c8
grfs ahwre <michael.d.keyes87@gmail.com>
2024-06-11 14:18:30 -0400

    basic crud

--- src/main/java/com/example/demo/controller/TutorialController.java
+++ src/main/java/com/example/demo/controller/TutorialController.java
@@ -4,12 +4,10 @@ import com.example.demo.model.Tutorial;
 import com.example.demo.service.TutorialService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tutorials")
@@ -19,7 +17,7 @@ public class TutorialController {
   private final TutorialService tutorialService;
 
   @GetMapping
-  public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
+  public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) final String title) {
 
     final List<Tutorial> tutorials = tutorialService.getAllTutorials(title);
 
@@ -28,5 +26,53 @@ public class TutorialController {
     }
 
     return ResponseEntity.ok(tutorials);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") final long id) {
+    final Optional<Tutorial> tutorial = tutorialService.getTutorialById(id);
+
+    return tutorial.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+  @GetMapping("/published")
+  public ResponseEntity<List<Tutorial>> getPublishedTutorials() {
+    final List<Tutorial> tutorials = tutorialService.getPublishedTutorials();
+
+    if (tutorials.isEmpty()) {
+      return ResponseEntity.noContent().build();
+    }
+
+    return ResponseEntity.ok(tutorials);
+  }
+
+  @PostMapping
+  public ResponseEntity<Tutorial> createTutorial(@RequestBody final Tutorial tutorial) {
+    return ResponseEntity.ok(tutorialService.createTutorial(tutorial));
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Tutorial> updateTutorial(@PathVariable("id") final long id, @RequestBody final Tutorial tutorial) {
+    final Tutorial updatedTutorial = tutorialService.updateTutorial(id, tutorial);
+
+    if (updatedTutorial == null) {
+      return ResponseEntity.notFound().build();
+    }
+
+    return ResponseEntity.ok(tutorialService.updateTutorial(id, tutorial));
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteTutorial(@PathVariable("id") final long id) {
+    tutorialService.deleteTutorial(id);
+
+    return ResponseEntity.noContent().build();
+  }
+
+  @DeleteMapping
+  public ResponseEntity<Void> deleteAllTutorials() {
+    tutorialService.deleteAllTutorials();
+
+    return ResponseEntity.noContent().build();
   }
 }
--- src/main/java/com/example/demo/service/TutorialService.java
+++ src/main/java/com/example/demo/service/TutorialService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +20,39 @@ public class TutorialService {
     } else {
       return tutorialRepository.findByTitleContaining(title);
     }
+  }
+
+  public Optional<Tutorial> getTutorialById(final long id) {
+    return tutorialRepository.findById(id);
+  }
+
+  public List<Tutorial> getPublishedTutorials() {
+    return tutorialRepository.findByPublished(true);
+  }
+
+  public Tutorial createTutorial(final Tutorial tutorial) {
+    return tutorialRepository.save(tutorial);
+  }
+
+  public Tutorial updateTutorial(final long id, final Tutorial tutorial) {
+    final Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
+
+    if (tutorialData.isPresent()) {
+      final Tutorial updatedTutorial = tutorialData.get();
+      updatedTutorial.setTitle(tutorial.getTitle());
+      updatedTutorial.setDescription(tutorial.getDescription());
+      updatedTutorial.setPublished(tutorial.isPublished());
+      return tutorialRepository.save(updatedTutorial);
+    } else {
+      return null;
+    }
+  }
+
+  public void deleteTutorial(final long id) {
+    tutorialRepository.deleteById(id);
+  }
+
+  public void deleteAllTutorials() {
+    tutorialRepository.deleteAll();
   }
 }
